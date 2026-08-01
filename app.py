0cf45aa630bbab3da627d423ecf48d948185db1d
Krish27-Gupta <krishguptazx9@gmail.com>
2026-08-01 10:42:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import os
 import time
 import numpy as np
 
+st.set_page_config(layout = "wide")
 #==============STEP 2 LOAD ENV and API KEYS===========
 st.title("Agentic PPT Generator")
 st.header("""User can generate, ppt, Images, and fetch latest news """)
@@ -111,7 +112,7 @@ tab1,tab2,tab3 = st.tabs(["Generate Image",
 
 user_input = st.text_area("Write Prompt & click Enter")
 
-if (user_input) & (leader_agent):
+if (user_input) :
     with tab1:
         if st.button("Click to Generate Image", key="Image-Button"):
             with st.spinner("Running Agent"):
